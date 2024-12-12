a95e324bad999d361232aca298d6b2e92474a2d3
diegossu44 <diegossu44@educastur.es>
2024-12-12 08:54:21 +0100

    Primer commit

---
+++ scripts/validate.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+
+});
